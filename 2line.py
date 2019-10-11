088100312416bcd74dda70797eccb788da48e5a2
ravi kumar <39921508+rk76912@users.noreply.github.com>
2019-10-11 16:43:53 +0530

    Add files via upload

---
+++ 2line.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+x1=[1,2,3]
+y1=[2,4,1]
+plt.plot(x1,y1,label="line1")
+x2=[1,2,3]
+y2=[4,1,3]
+plt.plot(x2,y2,label="line2")
+plt.xlabel('x -label')
+plt.ylabel('y -label')
+plt.title("my graph 2")
+plt.legend()
+plt.show()
