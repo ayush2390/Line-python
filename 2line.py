742b32629a80e8ece13b282839737aea160f4e90
souravjain540 <souravjain540@gmail.com>
2019-10-11 18:47:24 +0530

    updated
    line width added

--- 2line.py
+++ 2line.py
@@ -1,10 +1,10 @@
 from matplotlib import pyplot as plt
 x1=[1,2,3]
 y1=[2,4,1]
-plt.plot(x1,y1,label="line1")
+plt.plot(x1,y1,linewidth=2.0,label="line1")
 x2=[1,2,3]
 y2=[4,1,3]
-plt.plot(x2,y2,label="line2")
+plt.plot(x2,y2,linewidth=2.0,label="line2")
 plt.xlabel('x -label')
 plt.ylabel('y -label')
 plt.title("my graph 2")
