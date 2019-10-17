6e75e2d2b06bf509f8588455db80ecefd8bff3d4
ayush2390 <43995654+ayush2390@users.noreply.github.com>
2019-10-18 00:49:26 +0530

    Update 2line.py

--- 2line.py
+++ 2line.py
@@ -1,14 +1,14 @@
 from matplotlib import pyplot as plt
 import warnings
 warnings.filterwarnings('ignore')
-x1=[1,2,3]
-y1=[2,4,1]
-plt.plot(x1,y1,linewidth=2.0,label="line1")
-x2=[1,2,3]
-y2=[4,1,3]
-plt.plot(x2,y2,linewidth=2.0,label="line2")
+X1=[3,2,1]
+Y1=[1,4,2]
+plt.plot(X1,Y1,linewidth=3.0,label="1stline")
+X2=[2,1,3]
+Y2=[1,3,4]
+plt.plot(X2,Y2,linewidth=3.0,label="2ndline")
 plt.xlabel('x -label')
 plt.ylabel('y -label')
-plt.title("my graph 2")
+plt.title("my 2nd graph")
 plt.legend()
 plt.show()
